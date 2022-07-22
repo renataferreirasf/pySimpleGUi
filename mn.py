63657a1bf68eb2b0b1d3d2f7e96fa6e21898e1c2
Renata <renataferreirasf@gmail.com>
2022-07-22 09:13:02 -0300

    Keys adionadas

--- mn.py
+++ mn.py
@@ -1,13 +1,19 @@
 import PySimpleGUI as sg
 
-
 class Mn:
     def __init__(self):
         layout = [
-            [sg.Text('Nome'), sg.Input()],
-            [sg.Text('Idade'), sg.Input()],
-            [sg.Button()]
+            [sg.Text('Name'), sg.Input(key='name')],
+            [sg.Text('Age'), sg.Input(key='Age')],
+            [sg.Text('Providers?'), sg.Input()],
+            [sg.Checkbox('Gmail', key='Gmail'), sg.Checkbox('Outlook', key='Outlook'), sg.Checkbox('Yahoo', key='Yahoo')],
+            [sg.Button('Send')]
         ]
 
         janela = sg.Window("Dados do Unsuário").layout(layout)
-        self.button, sefl.values
+        self.button, self.values = janela.Read()
+
+    def Start(self):
+        print(self.values)
+tela = Mn()
+tela.Start()
